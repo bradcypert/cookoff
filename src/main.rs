4acac64517fb6d2d2a2513e576ee05cce9a2b53a
Brad Cypert <brad.cypert@gmail.com>
2022-11-30 18:45:22 -0500

    bulletproof this cookoff

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate serde;
 use std::env;
 
 use postgrest::Postgrest;
-use rocket::{serde::{Serialize, Deserialize}};
+use rocket::{serde::{Serialize, Deserialize}, State, http::Status};
 use rocket::form::Form;
 use rocket_dyn_templates::{Template, context};
 
@@ -24,23 +24,38 @@ struct SupabaseResp {
     name: String,
 }
 
-#[post("/", data = "<registrant>")]
-async fn post_index(registrant: Form<Registrant<'_>>) -> Template {
+struct TableNames {
+    registrant: String
+}
 
+#[post("/", data = "<registrant>")]
+async fn post_index(registrant: Form<Registrant<'_>>, client: &State<Postgrest>, tables: &State<TableNames>) -> Result<Template, Status> {
     let registrant = registrant.into_inner();
-    // TODO this is hella messy 
-    let client = Postgrest::new(env::var("supabase_rest_url").unwrap())
-        .insert_header("apikey", env::var("supabase_api_key").unwrap());
 
     if let Ok(json) = serde_json::to_string(&registrant) {
-        let ins = client.from(env::var("supabase_table_name").unwrap()).insert(json).execute().await.unwrap().text().await.unwrap();
-        let sr: Vec<SupabaseResp> = serde_json::from_str(ins.as_str()).unwrap();
-        let resp = sr.first().unwrap();
-        Template::render("index", context! { registered: "true", number: resp.id.to_string().as_str(), name: resp.name.as_str() })
+        let ins = client.from(&tables.registrant).insert(json).execute().await;
+        match ins {
+            Ok(response) =>  match response.text().await.ok() {
+                Some(body) => {
+                    let items: Vec<SupabaseResp> = serde_json::from_str(body.as_str()).unwrap();
+                    if let Some(resp) = items.first() {
+                        Ok(Template::render("index", context! { registered: "true", number: resp.id.to_string().as_str(), name: resp.name.as_str() }))
+                    } else {
+                        Err(Status::InternalServerError)
+                    }
+                },
+                _ => {
+                    // unable to get text from response body
+                    Err(Status::InternalServerError)
+                },
+            },
+            Err(_) => {
+                Err(Status::FailedDependency)
+            },
+        }
     } else {
-        Template::render("index", context! { error: "Something went wrong. Let Brad know and he'll fix it!" })
+        Ok(Template::render("index", context! { error: "Something went wrong. Let Brad know and he'll fix it!" }))
     }
-
 }
 
 
@@ -49,4 +64,9 @@ fn rocket() -> _ {
     rocket::build()
     .attach(Template::fairing())
     .mount("/", routes![index, post_index])
+    .manage(Postgrest::new(env::var("supabase_rest_url").unwrap())
+        .insert_header("apikey", env::var("supabase_api_key").unwrap()))
+    .manage(TableNames {
+        registrant: env::var("supabase_table_name").unwrap()
+    })
 }
